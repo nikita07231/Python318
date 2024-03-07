3ed569f8baca3540e9aea58587d3905f1e0646b9
nikita <nedorezovnikita7@mail.ru>
2024-03-07 21:15:41 +0500

    commit1

--- dz/dz1.py
+++ dz/dz1.py
@@ -1,11 +1,36 @@
-def func(a, *args):
-    for i in range(a):
-        d = []
-        d.append(args[i])
-    return d
+# s = 0
+#
+#
+# def outer(a, b, c):
+#     def inner(i, j):
+#         return i * j
+#
+#     global s
+#     s = 2 * (inner(a, b) + inner(a, c) + inner(b, c))
+#     return s
+#
+#
+# outer(2, 4, 6)
+# print(s)
+# outer(5, 8, 2)
+# print(s)
+# outer(1, 6, 8)
+# print(s)
 
 
-print(func(2, 3, 5))
+def outer(a, b, c):
+    s = 0
+
+    def inner(i, j):
+        nonlocal s
+        s += i * j
+
+    inner(a, b)
+    inner(a, c)
+    inner(b, c)
+    return 2 * s
 
 
-
+print(outer(2, 4, 6))
+print(outer(5, 8, 2))
+print(outer(1, 6, 8))
