b76e8bd6f751d923bf5fff48dc7f38ad4cc19905
nikita <nedorezovnikita7@mail.ru>
2024-04-14 18:26:10 +0500

    56

--- dz/dz1.py
+++ dz/dz1.py
@@ -1,9 +1,25 @@
-st = [-2, 3, 8, -11, -4, 6]
-n = 0
-for i in st:
-    if i < 0:
-        n += 1
-print(n)
+file = "text2.txt"
+
+f = open(file, "w")
+f.write("Замена строки в текстовом файле;\nизменить строку в списке;\nзаписать список в файл;\n")
+f.close()
+
+f = open(file, 'r')
+read_line = f.readlines()
+f.close()
+
+print(read_line)
+pos1 = int(input("pos1 = "))
+pos2 = int(input("pos2 = "))
+if 0 <= pos1 < len(read_line) and 0 <= pos2 < len(read_line):  # a, b = b, a
+    read_line[pos1], read_line[pos2] = read_line[pos2], read_line[pos1]
+else:
+    print("Такой строки нет")
+print(read_line)
+
+f = open(file, "w")
+f.writelines(read_line)
+f.close()
 
 
 
